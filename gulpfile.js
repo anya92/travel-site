eec2962e61984cce19bc584b2f0eac0fd63525d3
Ania Brajtkopf <aniabrajtkopf@gmail.com>
2017-08-01 18:51:05 +0200

    add gulpfile and style task

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp'),
+  watch = require('gulp-watch'),
+  postcss = require('gulp-postcss'),
+  autoprefixer = require('autoprefixer')
+  cssvars = require('postcss-simple-vars'),
+  nested = require('postcss-nested');
+
+gulp.task('default', () => {
+  console.log('gulp is running');
+});
+
+gulp.task('html', () => {
+  console.log('Imagine something useful being done to your HTML here.');
+});
+
+gulp.task('styles', () => {
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles'));
+});
+
+gulp.task('watch', () => {
+
+  watch('./app/index.html', () => {
+    gulp.start('html');
+  });
+
+  watch('./app/assets/styles/**/*.css', () => {
+    gulp.start('styles');
+  });
+
+});
